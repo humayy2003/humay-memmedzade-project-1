911aab2425995ace105e7a64b0e4aa43298aa1a1
humayy2003 <memmedzadehumay032@gmail.com>
2023-04-24 21:02:52 +0400

    yeni baslangic

--- project-messi/js/script.js
+++ project-messi/js/script.js
@@ -3,3 +3,13 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
   this.classList.toggle('is-active');
 });
+
+
+var counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 5000);
